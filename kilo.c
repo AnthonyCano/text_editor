82e5589baa33118decd48dacf184e5604c5944c9
Anthony Cano <132002947+AnthonyCano@users.noreply.github.com>
2025-02-01 23:11:23 -0800

    Adding more functionality. Finished step 26

--- kilo.c
+++ kilo.c
@@ -13,26 +13,33 @@
 /*** data ***/
 struct termios orig_termios;
 
+struct editorConfig {
+  struct termios orig_termios;
+};
+
+struct editorConfig E;
+
 void die(const char *s)
 {
-    perror(s);
-    exit(1);
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  perror(s);
+  exit(1);
 }
 
 
 /*** terminal ***/
-void disableRawMode()
-{
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+void disableRawMode(){
+if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 	die("tcsetattr");
 }
 
 void enableRawMode()
 {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcsetattr");
+  if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -43,25 +50,41 @@ void enableRawMode()
     if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
-char editorReadKey(){
+char editorReadKey() {
   int nread;
   char c;
-  
-  while((nread = read(STDIN_FILENO, &c, 1)) !=1){
+  while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN) die("read");
+  }
+  return c;
+}
+
+/*** input ***/
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'):
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    exit(0);
+    break;
   }
 }
 
-/*** inputs ***/
-
-void editorProcessKeypress(){
-  char c = editorReadKey();
-
-  switch(c){
-    case CTRL_KEY('q'):
-      exit(0);
-      break;
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < 24; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
   }
+}
+
+void editorRefreshScreen(){
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);
+
 }
 
 /*** init ***/
@@ -69,6 +92,7 @@ int main() {
   enableRawMode();
 
   while (1) {
+    editorRefreshScreen();
     editorProcessKeypress();
   }
   return 0;
